0280c82e5f1b1748f3ee163e9421e25613ad7908
Kostiantyn Pobozhni <k.pobozhny@gmail.com>
2019-04-09 20:50:41 +0300

    Auth. Part 1

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -17,13 +18,24 @@ class LoginForm extends Form {
       .label("Password")
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // Call the server
-    console.log("Submitted");
+    try {
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem('token', jwt);
+      window.location = '/'; // it will triger a full reload of application
+      // it is better then call: this.props.history.push('/'); - in this case we do not mount app with a valid info from local storage, where jwt token is stored
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
-
     return (
       <div>
         <h1>Login</h1>
